3a026b508c34475126af19549231f356c821bf08
Vaisakh <vaisakhpn78@gmail.com>
2023-07-31 21:06:20 +0530

    Banner and Poster finished

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import {API_KEY,imageUrl} from '../../constants/constants'
+import axios from '../../axios'
 import './Banner.css'
+
 const Banner = () => {
+  const [movie, setMovie] = useState()
+  useEffect(() => {
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      console.log(response.data.results[0])
+      const randomIndex = Math.floor(Math.random() * response.data.results.length);
+      const randomMovie = response.data.results[randomIndex];
+      setMovie(randomMovie)
+    });
+  }, []);
+  
   return (
-    <div className='banner'>
+    <div style={{backgroundImage:`url(${movie?imageUrl+movie.backdrop_path:""})`}} className='banner'>
         <div className='content'>
-            <h1 className='title'>Movie Name</h1>
+            <h1 className='title'>{movie?movie.title:""}</h1>
             <div className='banner-buttons'>
                 <button className='button'> play</button>
                 <button className='button'>My list</button>
             </div>
-            <h1 className='discription'>Placeholder Image Generators. A picture's worth a thousand words, so here's 21K worth of placeholder image generators to use in your mockups and designs.</h1>
+            <h1 className='discription'>{movie?movie.overview:""}</h1>
         </div>
         <div className='fade-bottom'></div>
     </div>
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const  baseUrl='https://api.themoviedb.org/3';
+export const API_KEY="a1a898596664fb5fa2ca7bb8d6da2d6b";
+export const imageUrl="https://image.tmdb.org/t/p/original"
