37a746af78df14f3589ec363ce0bb8572cf91966
Vaisakh <vaisakhpn78@gmail.com>
2023-07-31 11:32:45 +0530

    logo and avatar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import navBar from './NavBar.module.css'
+
+const NavBar = () => {
+  return (
+    <>
+    <div className={navBar.navbar}>
+        <img className={navBar.logo} src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt=" logo" />
+        <img className={navBar.avatar} src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="avatar" />
+    </div>
+    </>
+  )
+}
+
+export default NavBar
